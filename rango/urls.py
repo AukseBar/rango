8f96005bf01ef7e4f594225e1738a96cf8873744
auksebar <2036032b@student.gla.ac.uk>
2017-01-26 09:40:54 +0000

    completed chapter 6

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from rango import views
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-]
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    ]
